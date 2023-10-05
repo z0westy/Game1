cf1d9010e8054075b2600cd1c321239471a6ad5d
zirko <zirko@DESKTOP-RB8D8FO>
2023-10-05 23:00:20 +0300

    Добавьте файлы проекта.

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Game
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.background = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // background
+            // 
+            this.background.Image = ((System.Drawing.Image)(resources.GetObject("background.Image")));
+            this.background.Location = new System.Drawing.Point(0, -3000);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(4000, 4000);
+            this.background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.background.TabIndex = 2;
+            this.background.TabStop = false;
+            this.background.Click += new System.EventHandler(this.background_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(554, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(325, 699);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(74, 108);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(558, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(564, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 1000);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.background);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+
+            InitializeComponent();
+
+
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+
+            int speed = 10;
+
+            int formHeight = this.Height; // Высота окна
+            int formWidth = this.Width; // Длина окна
+
+            int background_x = background.Location.X;  // x верхнего и нижнего левого угла фона
+            int player_x = player.Location.X; // x верхнего и нижнего левого угла игрока
+            int background_x2 = background.Location.X+4000; // // x верхнего и нижнего правого угла фона
+            int player_x2 = player.Location.X+74; // x верхнего и нижнего левого угла игрока
+            int background_y = background.Location.Y; // y верхнего левого и правого угла фона
+            int player_y = player.Location.Y;  // y верхнего левого и правого угла игрока
+            int background_y2 = background.Location.Y+4000; // y нижнего левого и правого угла фона
+            int player_y2 = player.Location.Y + 108; // y нижнего левого и правого угла игрока
+
+            int player_xc = player.Location.Y + 37; // центральный x игрока
+            int player_yc = player.Location.Y + 54; // центральный y игрока
+
+
+
+            if ((e.KeyCode == Keys.W || e.KeyCode == Keys.Up) && (player_y > background_y) )
+            {
+                if ((player.Location.Y + 54 >= background.Location.Y + 5 + this.Height / 2) && (player.Location.Y + 54 <= background.Location.Y + 4000 - this.Height / 2))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        timer.Start();
+                        background.Top += 1;
+                    }
+                    timer.Stop();
+                }
+                else if ((player.Location.Y + 54 > background.Location.Y + 4000 - this.Height / 2) || (player.Location.Y + 54 < background.Location.Y + 5 + this.Height / 2))
+                {
+
+                    for (int i = 0; i< 10; i++)
+                    {
+                        timer.Start();
+                        if ((player.Location.Y + 54 > background.Location.Y + 4000 - this.Height / 2) || (player.Location.Y + 54 < background.Location.Y + 5 + this.Height / 2))
+                            player.Top -= 1;
+                    }
+                    
+                }
+                
+            }
+                
+   
+            if ((e.KeyCode == Keys.S || e.KeyCode == Keys.Down) && (player_y2 < background_y2))
+            {
+                label1.Text = Convert.ToString(player.Location.Y + 54);
+                label2.Text = Convert.ToString(background.Location.Y + this.Height / 2);
+                label3.Text = Convert.ToString(background.Location.Y + 4000 - this.Height / 2);
+
+                if ((player.Location.Y + 54 >= background.Location.Y + this.Height / 2) && (player.Location.Y + 54 <= background.Location.Y + 4000 - 5 - this.Height / 2))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        timer.Start();
+                        background.Top -= 1;
+                    }
+                    timer.Stop();
+                }
+                else if ((player.Location.Y + 54 > background.Location.Y + 4000 - 5 - this.Height / 2) || (player.Location.Y + 54 < background.Location.Y + this.Height / 2))
+                {
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        timer.Start();
+                        if ((player.Location.Y + 54 > background.Location.Y + 4000 - 5 - this.Height / 2) || (player.Location.Y + 54 < background.Location.Y + this.Height / 2))
+                            player.Top += 1;
+                    }
+                    timer.Stop();
+                }
+
+
+            }
+
+            if ((e.KeyCode == Keys.A || e.KeyCode == Keys.Left) && (player_x > background_x))
+            {
+                label1.Text = Convert.ToString(player.Location.X + 37);
+                label2.Text = Convert.ToString(background.Location.X + this.Width / 2);
+                label3.Text = Convert.ToString(background.Location.X + 4000 - this.Width / 2);
+                if ((player.Location.X + 37 >= background.Location.X + 5 + this.Width/2) && (player.Location.X + 37 <= background.Location.X + 4000 - this.Width / 2))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        timer.Start();
+                            background.Left += 1;
+
+                    }
+                    timer.Stop();
+                }
+                else if ((player.Location.X + 37 < background.Location.X + 5 + this.Width / 2) || (player.Location.X + 37 > background.Location.X + 4000 - this.Width / 2))
+                    for (int i = 0; i < 10; i++)
+                    {
+                        timer.Start();
+                        if ((player.Location.X + 37 < background.Location.X +5 + this.Width / 2) || (player.Location.X + 37 > background.Location.X + 4000 - this.Width / 2))
+                            player.Left -= 1;
+                    }
+                    timer.Stop();
+
+            }
+
+
+            if ((e.KeyCode == Keys.D || e.KeyCode == Keys.Right) && (player_x2 < background_x2))
+            {
+                label1.Text = Convert.ToString(player.Location.X + 37);
+                label2.Text = Convert.ToString(background.Location.X + this.Width / 2);
+                label3.Text = Convert.ToString(background.Location.X + 4000 - this.Width / 2);
+                if ((player.Location.X + 37 >= background.Location.X + this.Width / 2) && (player.Location.X + 37 <= background.Location.X + 4000 - 5 - this.Width / 2))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        timer.Start();
+                        background.Left -= 1;
+
+                    }
+                    timer.Stop();
+                }
+                else if ((player.Location.X + 37 < background.Location.X + this.Width / 2) || (player.Location.X + 37 > background.Location.X + 4000 -5  - this.Width / 2))
+                    for (int i = 0; i < 10; i++)
+                    {
+                        timer.Start();
+                        if ((player.Location.X + 37 < background.Location.X + this.Width / 2) || (player.Location.X + 37 > background.Location.X + 4000 - 5 - this.Width / 2))
+                            player.Left += 1;
+                    }
+                timer.Stop();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void background_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+                this.Close();
+
+        }
+    }
+}
